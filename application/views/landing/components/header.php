80f8b7b07d94bacd96ee2fea3429bacada8c209e
Digitintech admin <admin@digitintech.com>
2024-10-02 13:25:41 +0530

    updates on code

--- application/views/landing/components/header.php
+++ application/views/landing/components/header.php
@@ -1,41 +1,44 @@
-
 <?php $this->load->view("landing/components/top"); ?>
 
 <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
-    <main>
-      <div class="page-loader">
-        <div class="loader">Loading...</div>
-      </div>
-      <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>
-              <span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" style="display: flex;height: 55px;" href="<?= base_url(). 'home'?>"><img src="<?=base_url()?>assets/images/logo_icon.jpg" alt=""><img src="<?=base_url()?>assets/images/logo_text.jpg" alt=""></a>
-          </div>
-          <div class="collapse navbar-collapse" id="custom-collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <!-- <li class=""><a class="" href="<?= base_url() . 'home' ?>" data-toggle="dropdown">Home</a>
+	<main>
+		<div class="page-loader">
+			<div class="loader">Loading...</div>
+		</div>
+		<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span
+							class="sr-only">Toggle navigation</span><span class="icon-bar"></span>
+						<span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand"
+						style="display: flex;height: 60px;" href="<?= base_url(). 'home'?>"><img
+							src="<?=base_url()?>assets/landing/assets/images/new/logo.png" alt=""></a>
+				</div>
+				<div class="collapse navbar-collapse" id="custom-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<!-- <li class=""><a class="" href="<?= base_url() . 'home' ?>" data-toggle="dropdown">Home</a>
               </li> -->
-              <li class=""><a class="" href="<?= base_url() . 'about' ?>" data-toggle="">About</a>
-              </li>
-              <li class=""><a class="" href="<?= base_url() . 'products' ?>" data-toggle="">Products</a>
-              </li>
-               <li class=""><a class="" href="<?= base_url() . 'services' ?>" data-toggle="">Services</a>
-              </li>
-                <li class=""><a class="" href="<?= base_url() . 'projects' ?>" data-toggle="">Our Projects</a> 
-              </li>
-                <li class=""><a class="" href="<?= base_url() . 'gallery' ?>" data-toggle="">Gallery</a> 
-              </li>
-              </li>
-              <li class=""><a class="" href="<?= base_url() . 'clients' ?>" data-toggle="">Clients</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-			<?php if (!empty($success_message)): ?>
-        <div class="alert alert-success" role="alert">
-                  <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button><strong><?= $success_message; ?></strong> 
-                </div>
-    <?php endif; ?>
-			
+						<li class=""><a class="" href="<?= base_url() . 'about' ?>" data-toggle="">About</a>
+						</li>
+						<li class=""><a class="" href="<?= base_url() . 'products' ?>" data-toggle="">Products</a>
+						</li>
+						<li class=""><a class="" href="<?= base_url() . 'services' ?>" data-toggle="">Services</a>
+						</li>
+						<li class=""><a class="" href="<?= base_url() . 'projects' ?>" data-toggle="">Our Projects</a>
+						</li>
+						<li class=""><a class="" href="<?= base_url() . 'gallery' ?>" data-toggle="">Gallery</a>
+						</li>
+						</li>
+						<li class=""><a class="" href="<?= base_url() . 'clients' ?>" data-toggle="">Clients</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<?php if (!empty($success_message)): ?>
+		<div class="alert alert-success" role="alert">
+			<button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button><strong>
+				<?= $success_message; ?>
+			</strong>
+		</div>
+		<?php endif; ?>
--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -1,20 +1,20 @@
 <?php $this->load->view("landing/components/header"); ?>
 
 <section class="home-section home-full-height bg-dark-30" id="home"
-	data-background="<?php echo base_url(); ?>assets/landing/assets/images/section-5.jpg">
+	data-background="<?php echo base_url(); ?>assets/landing/assets/images/new/1.jpeg">
 	<!-- <div class="video-player" data-property="{videoURL:'https://www.youtube.com/watch?v=bNucJgetMjE', containment:'.home-section', startAt:18, mute:false, autoPlay:true, loop:true, opacity:1, showControls:false, showYTLogo:false, vol:25}"></div>
         <div class="video-controls-box">
           <div class="container">
             <div class="video-controls"><a class="fa fa-volume-up" id="video-volume" href="#">&nbsp;</a><a class="fa fa-pause" id="video-play" href="#">&nbsp;</a></div>
           </div>
         </div> -->
-	<div class="titan-caption">
+	<!-- <div class="titan-caption">
 		<div class="caption-content">
 			<div class="font-alt mb-30 titan-title-size-1">Hello &amp; welcome</div>
 			<div class="font-alt mb-40 titan-title-size-4">We are Majestic</div><a
 				class="section-scroll btn btn-border-w btn-round" href="#about">Learn More</a>
 		</div>
-	</div>
+	</div> -->
 </section>
 <div class="main">
 	<section class="module" id="story">
