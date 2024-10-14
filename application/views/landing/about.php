826dda742461d8bd6caa12e9a70cf34dc872a4a4
Digitintech admin <admin@digitintech.com>
2024-10-14 14:44:56 +0530

    updates

--- application/views/landing/about.php
+++ application/views/landing/about.php
@@ -129,86 +129,75 @@
                     <div class="row justify-content-center">
                         <div class="col-12">
                             <div class="thm-header text-center">
-                                <p class="c1 pb-10">team members</p>
-                                <h1 class="c3">Meet Our Experts</h1>
+                                <p class="c1 pb-10">Services</p>
+                                <h1 class="c3">Our Services</h1>
                             </div><!-- /.thm-header -->
                         </div><!-- /.col-12 -->
-                        <div class="col-md-4">
-                            <div class="member-card bg-white">
-                                <figure>
-                                    <div class="text-md-right text-center">
-                                        <img src="<?php echo base_url(); ?>assets/landing/img/team-1-1.jpg" alt="Team Member">
-                                    </div>
-                                    <figcaption>
-                                        <ul>
-                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                            <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-                                        </ul>
-                                        <div class="member-info f1">
-                                            <h2 class="fw-6 c3"><a href="#">Mike Hardson</a></h2>
-                                            <p class="fw-6">Technician</p>
-                                        </div><!-- /.member-info -->
-                                    </figcaption>
-                                </figure>
-                            </div><!-- /.member-card -->
-                        </div><!-- /.col-lg-4 -->
-                        <div class="col-md-4">
-                            <div class="member-card bg-white">
-                                <figure>
-                                    <div class="text-md-right text-center">
-                                        <img src="<?php echo base_url(); ?>assets/landing/img/team-1-2.jpg" alt="Team Member">
-                                    </div>
-                                    <figcaption>
-                                        <ul>
-                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                            <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-                                        </ul>
-                                        <div class="member-info f1">
-                                            <h2 class="fw-6 c3"><a href="#">Mike Hardson</a></h2>
-                                            <p class="fw-6">Technician</p>
-                                        </div><!-- /.member-info -->
-                                    </figcaption>
-                                </figure>
-                            </div><!-- /.member-card -->
-                        </div><!-- /.col-lg-4 -->
-                        <div class="col-md-4">
-                            <div class="member-card bg-white">
-                                <figure>
-                                    <div class="text-md-right text-center">
-                                        <img src="<?php echo base_url(); ?>assets/landing/img/team-1-3.jpg" alt="Team Member">
-                                    </div>
-                                    <figcaption>
-                                        <ul>
-                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                            <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-                                        </ul>
-                                        <div class="member-info f1">
-                                            <h2 class="fw-6 c3"><a href="#">Mike Hardson</a></h2>
-                                            <p class="fw-6">Technician</p>
-                                        </div><!-- /.member-info -->
-                                    </figcaption>
-                                </figure>
-                            </div><!-- /.member-card -->
-                        </div><!-- /.col-lg-4 -->
+						<?php foreach ($services as $product): ?>
+                        	<!-- <a href="#"><img src="" alt="Partners"></a> -->
+							<div class="col-md-4">
+								<div class="member-card bg-white">
+									<figure>
+										<div class="text-md-right text-center">
+											<img src="<?= base_url('' . $product['image']); ?>" alt="Team Member">
+										</div>
+										<figcaption>
+											<div class="member-info f1">
+												<h2 class="fw-6 c3"><a href="#"><?= $product['title']; ?></a></h2>
+												<p class="fw-6"><?= $product['content']; ?></p>
+											</div><!-- /.member-info -->
+										</figcaption>
+									</figure>
+								</div><!-- /.member-card -->
+							</div><!-- /.col-lg-4 -->
+						<?php endforeach; ?>
                     </div><!-- /.row -->
                 </div><!-- /.container -->
             </div><!-- /.sec-team -->
+			<div class="testimony text-white text-center">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-12">
+							<div class="thm-header text-center text-white">
+								<p class="pb-10">What Sets Us Apart</p>
+								<h1>Our Products</h1>
+							</div><!-- /.thm-header -->
+						</div><!-- /.col-12 -->
+						<div class="col-xl-8 col-lg-8 col-md-10">
+							<div class="rating-area">
+								<div class="testi-pager d-flex justify-content-center align-items-end mb-55" id="testi-pager">
+								<?php foreach ($products as $key=>$product): ?>
+									<a href="#" class="pager-item active" data-slide-index="<?= $key; ?>">
+										<div class="testi-thumb">
+											<img src="<?= base_url('' . $product['image']); ?>" alt="rating" title="">
+										</div>
+									</a>
+								<?php endforeach; ?>
+									
+								</div>
 
-            
-            
+								<!--Slider-->
+								<ul class="rating-slider f1">
+								<?php foreach ($products as $key=>$product): ?>
+									<li class="slide-item">
+										<div class="slide-text">
+											<p><?= $product['content']; ?></p>
+											<h4 class="mt-30"><?= $product['title']; ?></h4>
+										</div>
+									</li>
+								<?php endforeach; ?>
+								</ul>
+							</div><!-- /.rating-area -->
+						</div><!-- /.col-xl-6 col-lg-8 col-md-10 offset-xl-3 offset-lg-2 offset-md-1 offset-sm-0 -->
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</div><!-- /.testimony -->
             <div class="partners bg4">
                 <div class="container">
                     <div class="partner-carousel owl-carousel">
 						<?php foreach ($clients as $product): ?>
                         	<a href="#"><img src="<?= base_url('' . $product['image']); ?>" alt="Partners"></a>
 						<?php endforeach; ?>
-                        <!-- <a href="#"><img src="<?php echo base_url(); ?>assets/landing/img/partners/01.png" alt="Partners"></a>
-                        <a href="#"><img src="<?php echo base_url(); ?>assets/landing/img/partners/01.png" alt="Partners"></a>
-                        <a href="#"><img src="<?php echo base_url(); ?>assets/landing/img/partners/01.png" alt="Partners"></a>
-                        <a href="#"><img src="<?php echo base_url(); ?>assets/landing/img/partners/01.png" alt="Partners"></a> -->
                     </div><!-- /.partner-carousel -->
                 </div><!-- /.container -->
             </div><!-- /.partners -->
